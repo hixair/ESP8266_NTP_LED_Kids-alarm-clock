95322bbbae05b9754f922c9c48b27476456e40f2
Xavier Rotivel <xavtek@gmail.com>
2020-01-26 16:59:41 +0100

    Update sketch_NTP_LED.ino

--- sketch_NTP_LED.ino
+++ sketch_NTP_LED.ino
@@ -3,7 +3,7 @@
 #include <WiFiUdp.h>
 
 const char *ssid     = "SSID";
-const char *password = "password";
+const char *password = "Password";
 
 WiFiUDP ntpUDP;
 
@@ -19,10 +19,13 @@ void setup(){
   pinMode(LED_BUILTIN, OUTPUT);     // Initialize the LED_BUILTIN pin as an output
   pinMode(2, OUTPUT);               // Initialize GPIO2 pin as an output
   Serial.begin(115200);
+  Serial.print ( "Connecting " );
   WiFi.begin(ssid, password);
 
   while ( WiFi.status() != WL_CONNECTED ) {
-    delay ( 500 );
+    digitalWrite(2, HIGH);                              //Switch led OFF
+    delay ( 500 );                                      //Wait
+    digitalWrite(2, LOW);                               //Switch led ON (until connected)
     Serial.print ( "." );
   }
 
@@ -33,20 +36,18 @@ void setup(){
 void loop() {
   digitalWrite(2,HIGH);     //Switching the led Off
   timeClient.update();      //Updating time from NTP
-  Serial.print ( "syncing time" );
- 
-  
+
   int t = timeClient.getHours() * 100 + timeClient.getMinutes();   //present the time in another form to simplyfy operations
   Serial.println(t);
 
-  if ((t >= 800 && t <= 830) || (t >= 1530 && t <= 1630)) {.      //Add condition, if time is equal or greater than 8:00, switch leds on. NEVER put 0800 instead 800.
+  if ((t >= 800 && t <= 830) || (t >= 1600 && t <= 1630)) {       //Add condition, if time is equal or greater than 8:00, switch leds on. NEVER put 0800 instead 800.
     digitalWrite(LED_BUILTIN, LOW);                               //Switch leds ON
     digitalWrite(2, LOW);
   } else {
     digitalWrite(LED_BUILTIN, HIGH);
     digitalWrite(2, HIGH);
   }
-  
+  delay ( 500 );
   Serial.println(timeClient.getFormattedTime());                 //print time on serial
-
+  delay ( 500 );
 }
